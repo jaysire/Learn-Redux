1640bb09a299474f50d76b04f988ab1454c792a4
Josiah Maina <josiah5115@gmail.com>
2018-06-06 19:07:20 -0600

    Pure Functions initial

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,9 +1,13 @@
 var redux = require('redux');
 
-console.log('Starting Redux example now! <br/> Get ready to RRRUUUMMMMBBBBBLLLLLEEEE!!!!');
+console.log('Starting Redux example now! Get ready to RRRUUUMMMMBBBBBLLLLLEEEE!!!!');
 
-console.log('Today is hump day!')
+// Pure Functions
+function add (a, b) {
+  return a + b;
+};
 
-console.log('When will you link up');
-
-
+// its always going to return the same result given the same input.
+// There are no side effects; i.e it doesnt rely on variables defined above it and:
+// it does not change any values outside of itself. i.e it dosent update or use any varaibles.
+// Pure funcs cant have any asynchronous requests: no promises or callbacks.

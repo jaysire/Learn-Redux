1b941a5077317256789e4e0b061a00f73962aba0
Josiah Maina <josiah5115@gmail.com>
2018-06-07 02:17:27 -0600

    passing action thru reducer func

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,24 +2,69 @@ var redux = require('redux');
 
 console.log('Starting Redux example now! Get ready to RRRUUUMMMMBBBBBLLLLLEEEE!!!!');
 
+// *** DISPATCHING & HANDLING ACTIONS ***;
+
+const reducer = (state = {name: 'Anonymous'}, action) => {
+  console.log('This is our NEW ACTION!=>>', action);
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+      ...state,
+      name: action.name
+    };
+  default:
+    return state;
+  }
+};
+const store = redux.createStore(reducer);
+
+const currentState = store.getState();
+console.log('This is our CURRENT STATE!=>>', currentState);
+
+// we need to Dispatch Actions in-order to update the State;
+// Actions are simply Objects the only requirement: they MUST have the 'type' property: which will rep the Action Name;
+//***N/B*** ALL app names are ALL in UPPERCASES and use Under_Score to tie more than one name together like seen below:
+
+const action = {
+  type: 'CHANGE_NAME',
+  name: 'Josiah'
+};
+
+// now we have our Action, we need to Dispatch it to our Store.
+// we use the dispatch method which takes one argument: i.e your Action Object;
+store.dispatch(action);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Pure Function is called reducer in Redux
 // reducer takes your Existing State and the Action to occur as arguments/ parameters, & computs the NEW State;
 // reducer is called by Redux and takes two arguments and then it gets passed into our store; It requires this function/ the reducer.
 // We also need to add a default as we initialize our store; in our case we'll set our default to anonymous;
 // this is the syntax to creating default arguments/ parameters that get passed thru our reducer/ pure func.
 
-// create simple reducer;
-const reducer = (state = {name: 'Anonymous'}, action) => {
-  // state = state || {name: 'Anonymous'};
+// // create simple reducer;
+// const reducer = (state = {name: 'Anonymous'}, action) => {
+//   // state = state || {name: 'Anonymous'};
 
-  return state;
-};
-// create Store;
-const store = redux.createStore(reducer);
+//   return state;
+// };
+// // create Store;
+// const store = redux.createStore(reducer);
 
-// get/ fetch the state;
-const currentState = store.getState();
-console.log('currentState', currentState);
+// // get/ fetch the state;
+// const currentState = store.getState();
+// console.log('currentState', currentState);
 
 
 // reducer: takes existing state and the action as arguments and computes the New State.
@@ -30,7 +75,7 @@ console.log('currentState', currentState);
 // that means the state would have our searched text before we trigger a change to that text.
 // The Second parameter is the Action that was triggered.
 // Our job is to put these two actions together and return soemthing new.
-// We also want to have a defaul State incase there is nothing passed.
+// We also want to have a default State incase there is nothing passed.
 
 // THIS IS THE ES5 SYNTAX:
 

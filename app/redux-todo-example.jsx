07717716ac8ad00a075c92817429e8dd75ce0e81
Josiah Maina <josiah5115@gmail.com>
2018-06-07 01:39:50 -0600

    solution to redux todo challange 1

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -1,3 +1,18 @@
 const redux = require('redux');
 
 console.log('Here we go again!! Redux-todo-example! baby!!');
+
+// since we need to pass more than one argument, we can start by saving it to a variable and passing the variable instead thru our reducer func
+const stateDefault = {
+  searchText: '',
+  showCompleted: false,
+  todos: []
+};
+
+const reducer = (state = stateDefault, action) => {
+  return state;
+};
+
+const store = redux.createStore(reducer);
+
+console.log('currentState', store.getState());

ac41287fe879d61ac11ac0585c94bd7c9a59a5ac
Josiah Maina <josiah5115@gmail.com>
2018-06-08 16:49:14 -0600

    project reconciliation

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,119 +1,116 @@
-const redux = require('redux');
+var redux = require('redux');
 
 console.log('Starting redux tutorial example below!');
 
-const stateDefault = {
-	name: 'Anonymous',
-	hobbies: [],
-	movies: [],
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
 };
-const nextHobbyId = 1;
-const nextMovieId = 1;
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action) => {
+  // state = state || {name: 'Anonymous'};
 
-const nameReducer = (state = 'Anonymous', action) => {
-	switch (action.type) {
-		case 'CHANGE_NAME':
-			return action.name;
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+      };
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+      }
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: nextMovieId++,
+            title: action.title,
+            genre: action.genre
+          }
+        ]
+      }
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie.id !== action.id)
+      }
+    default:
+      return state;
+  }
 };
-
-const hobbiesReducer = (state = [], action) => {
-	switch (action.type) {
-		case 'ADD_HOBBY':
-			return [
-				...state,
-				{
-					id: nextHobbyId = nextHobbyId + 1,
-					hobby: action.hobby,
-				},
-			];
-		case 'REMOVE_HOBBY':
-			return state.filter(hobby => hobby.id !== action.id);
-		default:
-			return state;
-	}
-};
-
-const moviesReducer = (state = [], action) => {
-	switch (action.type) {
-		case 'ADD_MOVIE':
-			return [
-				...state,
-				{
-					id: nextMovieId = nextMovieId + 1,
-					title: action.title,
-					genre: action.genre, 
-				},
-			];
-		case 'REMOVE_MOVIE':
-			return state.filter(movie => movie.id !== action.id);
-		default:
-			return state;
-	}
-};
-
-const reducer = redux.combineReducers({
-	name: nameReducer,
-	hobbies: hobbiesReducer,
-	movies: moviesReducer,
-});
-
-const store = redux.createStore(reducer, redux.compose(window.devToolsExtension ? window.devToolsExtension() : f => f));
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
 // Subscribe to changes
-const unsubscribe = store.subscribe(() => {
-	const state = store.getState();
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
 
-	console.log('Name is', state.name);
-	document.getElementById('app').innerHTML = state.name;
+  console.log('Name is', state.name);
+  document.getElementById('app').innerHTML = state.name;
 
-	console.log('New state', store.getState());
+  console.log('New state', store.getState());
 });
 // unsubscribe();
 
-const currentState = store.getState();
+var currentState = store.getState();
 console.log('currentState', currentState);
 
 store.dispatch({
-	type: 'CHANGE_NAME',
-	name: 'Ras Joh',
+  type: 'CHANGE_NAME',
+  name: 'Josiah'
 });
 
 store.dispatch({
-	type: 'ADD_HOBBY',
-	hobby: 'Camping',
+  type: 'ADD_HOBBY',
+  hobby: 'Camping'
 });
 
 store.dispatch({
-	type: 'ADD_HOBBY',
-	hobby: 'Soccer',
+  type: 'ADD_HOBBY',
+  hobby: 'Soccer'
 });
 
 store.dispatch({
-	type: 'REMOVE_HOBBY',
-	id: 2,
+  type: 'REMOVE_HOBBY',
+  id: 2
 });
 
 store.dispatch({
-	type: 'CHANGE_NAME',
-	name: 'Miriam',
+  type: 'CHANGE_NAME',
+  name: 'Miriam'
 });
 
 store.dispatch({
-	type: 'ADD_MOVIE',
-	title: 'Red Rose',
-	genre: 'Horror',
+  type: 'ADD_MOVIE',
+  title: 'Rose Red',
+  genre: 'Horror'
 });
 
 store.dispatch({
-	type: 'ADD_MOVIE',
-	title: 'Wedding Crashers',
-	genre: 'Comedy',
+  type: 'ADD_MOVIE',
+  title: 'Wedding Crashers',
+  genre: 'Comedy'
 });
 
 store.dispatch({
-	type: 'REMOVE_MOVIE',
-	id: 1,
+  type: 'REMOVE_MOVIE',
+  id: 1
 });

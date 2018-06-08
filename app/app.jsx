9dcadda93556791608af56bd5423ce37b79f0ff1
Josiah Maina <josiah5115@gmail.com>
2018-06-07 20:59:38 -0600

    Redux-Action, Add to Array Challange

--- app/app.jsx
+++ app/app.jsx
@@ -1,16 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var { Route, Router, IndexRoute, hashHistory } = require('react-router');
 
 // Load foundation
 $(document).foundation();
 
 // App css
-require('style!css!sass!applicationStyles')
+require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <p>This is our Boilerplate 3 Project template for the new application project.</p>,
-  document.getElementById('app')
+	<p>This is our Boilerplate 3 Project template for the new application project.</p>,
+	document.getElementById('app')
 );
 
 require('./redux-example.jsx');
--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -4,45 +4,59 @@ console.log('Starting redux example tutorials below!!');
 
 // when initializing our default we leave the hobbies array empty. No need initializing it with values since we will update them later in our code;
 const stateDefault = {
-  name: 'Anonymous',
-  hobbies: [],
+	name: 'Anonymous',
+	hobbies: [],
+	movies: [],
 };
 
+var nextMovieId = 1;
 var nextHobbyId = 1;
 const reducer = (state = stateDefault, action) => {
-  switch (action.type) {
-    case 'CHANGE_NAME':
-      return {
-      ...state,
-      name: action.name
-    };
-    case 'ADD_HOBBY':
-      return {
-        ...state,
-        hobbies: [
-          ...state.hobbies,
-          {
-            id: nextHobbyId++,
-            hobby: action.hobby
-          }
-        ]
-      }
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case 'CHANGE_NAME':
+			return {
+				...state,
+				name: action.name,
+			};
+		case 'ADD_HOBBY':
+			return {
+				...state,
+				hobbies: [
+					...state.hobbies,
+					{
+						id: nextHobbyId++,
+						hobby: action.hobby,
+					},
+				],
+			};
+		case 'ADD_MOVIE':
+			return {
+				...state,
+				movies: [
+					...state.movies,
+					{
+						id: nextMovieId++,
+						title: action.title,
+						genre: action.genre,
+					},
+				],
+			};
+		default:
+			return state;
+	}
 };
 
-const store = redux.createStore(reducer, redux.compose(
-  window.devToolsExtension ? window.devToolsExtension() : f => f
-));
+const store = redux.createStore(reducer, redux.compose(window.devToolsExtension ? window.devToolsExtension() : f => f));
 
 // Subscribe to Changes:
 // The Subscribe method returns a unsubscribe function that we can initlize and save the value to it and call/ invoke it when we need to unsubscribe to actions.
 const unsubscribe = store.subscribe(() => {
-  const state = store.getState();
+	const state = store.getState();
 
-  console.log('Name is', state.name);
-  document.getElementById('app').innerHTML = state.name;
+	console.log('Name is', state.name);
+	document.getElementById('app').innerHTML = state.name;
+
+	console.log('This is our New State!=>>', store.getState());
 });
 // unsubscribe();
 
@@ -50,16 +64,22 @@ const currentState = store.getState();
 console.log('CurrentState', currentState);
 
 store.dispatch({
-  type: 'CHANGE_NAME',
-  name: 'Ras Joh'
+	type: 'CHANGE_NAME',
+	name: 'Ras Joh',
 });
 
 store.dispatch({
-  type: 'ADD_HOBBY',
-  hobby: 'Camping'
+	type: 'ADD_HOBBY',
+	hobby: 'Camping',
 });
 
 store.dispatch({
-  type: 'CHANGE_NAME',
-  name: 'Mildred'
+	type: 'CHANGE_NAME',
+	name: 'Mildred',
+});
+
+store.dispatch({
+	type: 'ADD_MOVIE',
+	title: 'Rose Red',
+	genre: 'Horror',
 });

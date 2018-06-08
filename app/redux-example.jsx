d7f3e8ae528fe9b1e3b5fdeecb3bab0e20e6c87f
Josiah Maina <josiah5115@gmail.com>
2018-06-08 13:14:20 -0600

    multiple reducers

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,92 +1,80 @@
 const redux = require('redux');
 
-console.log('Starting redux example tutorials below!!');
+console.log('Starting redux tutorial example below!');
 
-// when initializing our default we leave the hobbies array empty. No need initializing it with values since we will update them later in our code;
 const stateDefault = {
 	name: 'Anonymous',
 	hobbies: [],
 	movies: [],
 };
+const nextHobbyId = 1;
+const nextMovieId = 1;
 
-var nextMovieId = 1;
-var nextHobbyId = 1;
-const oldReducer = (state = stateDefault, action) => {
+const nameReducer = (state = 'Anonymous', action) => {
 	switch (action.type) {
 		case 'CHANGE_NAME':
-			return {
-				...state,
-				name: action.name,
-			};
-		case 'ADD_HOBBY':
-			return {
-				...state,
-				hobbies: [
-					...state.hobbies,
-					{
-						id: nextHobbyId++,
-						hobby: action.hobby,
-					},
-				],
-			};
-		case 'REMOVE_HOBBY':
-			return {
-				...state,
-				hobbies: state.hobbies.filter(hobby => hobby.id !== action.id),
-			};
-		case 'ADD_MOVIE':
-			return {
-				...state,
-				movies: [
-					...state.movies,
-					{
-						id: nextMovieId++,
-						title: action.title,
-						genre: action.genre,
-					},
-				],
-			};
-		case 'REMOVE_MOVIE':
-			return {
-				...state,
-				movies: state.movies.filter(movie => movie.id !== action.id),
-			};
+			return action.name;
 		default:
 			return state;
 	}
 };
 
-const nameReducer = (state = 'Anonymous',action) => {
-  switch (action.type) {
-    case 'CHANGE_NAME':
-      return action.name;
-    default:
-      return state;
-      // this was we only return what the function cares about. 
-      // We return a string & our code is simplified.
-  }
+const hobbiesReducer = (state = [], action) => {
+	switch (action.type) {
+		case 'ADD_HOBBY':
+			return [
+				...state,
+				{
+					id: nextHobbyId = nextHobbyId + 1,
+					hobby: action.hobby,
+				},
+			];
+		case 'REMOVE_HOBBY':
+			return state.filter(hobby => hobby.id !== action.id);
+		default:
+			return state;
+	}
+};
+
+const moviesReducer = (state = [], action) => {
+	switch (action.type) {
+		case 'ADD_MOVIE':
+			return [
+				...state,
+				{
+					id: nextMovieId = nextMovieId + 1,
+					title: action.title,
+					genre: action.genre, 
+				},
+			];
+		case 'REMOVE_MOVIE':
+			return state.filter(movie => movie.id !== action.id);
+		default:
+			return state;
+	}
 };
 
 const reducer = redux.combineReducers({
-  name: nameReducer
-})
+	name: nameReducer,
+	hobbies: hobbiesReducer,
+	movies: moviesReducer,
+});
 
 const store = redux.createStore(reducer, redux.compose(window.devToolsExtension ? window.devToolsExtension() : f => f));
 
-// Subscribe to Changes:
-// The Subscribe method returns a unsubscribe function that we can initlize and save the value to it and call/ invoke it when we need to unsubscribe to actions.
+// Subscribe to changes
 const unsubscribe = store.subscribe(() => {
 	const state = store.getState();
 
 	console.log('Name is', state.name);
 	document.getElementById('app').innerHTML = state.name;
 
-	console.log('This is our New State!=>>', store.getState());
+	console.log('New state', store.getState());
 });
 // unsubscribe();
 
 const currentState = store.getState();
-console.log('CurrentState', currentState);
+console.log('currentState', currentState);
 
 store.dispatch({
 	type: 'CHANGE_NAME',
@@ -100,12 +88,7 @@ store.dispatch({
 
 store.dispatch({
 	type: 'ADD_HOBBY',
-	hobby: 'Swimming',
-});
-
-store.dispatch({
-	type: 'CHANGE_NAME',
-	name: 'Miriam',
+	hobby: 'Soccer',
 });
 
 store.dispatch({
@@ -114,14 +97,19 @@ store.dispatch({
 });
 
 store.dispatch({
+	type: 'CHANGE_NAME',
+	name: 'Miriam',
+});
+
+store.dispatch({
 	type: 'ADD_MOVIE',
-	title: 'Rose Red',
+	title: 'Red Rose',
 	genre: 'Horror',
 });
 
 store.dispatch({
 	type: 'ADD_MOVIE',
-	title: 'Weddig Crashers',
+	title: 'Wedding Crashers',
 	genre: 'Comedy',
 });
 

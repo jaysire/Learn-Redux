46859a008da9b1c456d432942d4c784f46b9adb3
Josiah Maina <josiah5115@gmail.com>
2018-06-08 19:36:37 -0600

    map reducer func complete set-up

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -103,7 +103,18 @@ const removeMovie = (id) => {
 // ----------------------------------------
 const mapReducer = (state = {isFetching: false, url: undefined}, action) => {
   switch (action,type) {
-    
+    case 'START_LOCATION_FETCH':
+    return {
+      isFetching: true,
+      url: undefined
+    };
+    case 'COMPLETE_LOCATION_FETCH':
+      return {
+        isFetching: false,
+        url: action.url
+    };
+    default:
+      return state;
   }
 }
 
